27b58fc0b00054333ea40bd37ce743523dbefbc1
Guilherme Leon Berno de Jesus <guileon1234@gmail.com>
2013-04-28 12:05:29 -0300

    fiz a primeira parte do segundo trabalho
    FALTA TESTAR, to testando agora

---
+++ sisop2.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define RIGHT philosopher_number
+#define LEFT philosopher_number-1
+#define RANDOM (rand() % 10)+1
+
+sem_t * mutex;
+int n_mutex;
+
+void philosopher(int philosopher_number)
+{
+	while(1)
+	{
+		
+		printf("filosfo %d tentando comer\n",philosopher_number);
+		sleep(RANDOM);
+		if(philosopher_number==0)
+		{
+			sem_wait(&(mutex[n_mutex-1]));
+			sem_wait(&(mutex[RIGHT]));
+			printf("filosofo %d comendo\n",philosopher_number);
+			sem_post(&(mutex[n_mutex-1]));
+			sem_post(&(mutex[RIGHT]));
+		}
+		else
+		{
+			sem_wait(&(mutex[RIGHT]));
+			sem_wait(&(mutex[LEFT]));
+			printf("filosofo %d comendo\n",philosopher_number);
+			sem_post(&(mutex[RIGHT]));
+			sem_post(&(mutex[LEFT]));
+		}
+		printf("filosfo %d pensando\n",philosopher_number);
+		sleep(RANDOM);
+	}
+
+		
+}
+
+int main(int argc, char** argv)
+{
+	fprintf(stderr,"oba\n");
+	pthread_t tid;
+	int i;
+	int philo_number = atoi(argv[1]);
+	
+	mutex = calloc(philo_number,sizeof(sem_t));
+	n_mutex = philo_number;
+		
+	for(i=0 ; i < philo_number ; i++)
+		sem_init(&mutex[i], 0, 1);
+
+	for(i=0 ; i < philo_number ; i++)
+		pthread_create(&tid,NULL,&philosopher,i);
+	getchar();
+}
