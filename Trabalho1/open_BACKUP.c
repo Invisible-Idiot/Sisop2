28744e9bbec85e60d156676ea69b9d66d4135d5c
Guilherme Leon Berno de Jesus <guileon1234@gmail.com>
2013-06-06 17:17:18 -0300

    ?

--- Trabalho1/open_BACKUP.c
+++ Trabalho1/open_BACKUP.c
@@ -6,25 +6,6 @@
 #define EQUAL '='
 #define TERMINATOR_CHAR '\0'
 #define SPACE ' '
-
-int is_int(char c)
-{
-	if(
-	c!='0'&&
-	c!='1'&&
-	c!='2'&&
-	c!='3'&&
-	c!='4'&&
-	c!='5'&&
-	c!='6'&&
-	c!='7'&&
-	c!='8'&&
-	c!='9') 
-		return 0;
-	else
-		return 1;
-}
-
 int ** open(char* filename, int* nLine, int* nCol)
 {
 	
@@ -80,70 +61,50 @@ int ** open(char* filename, int* nLine, int* nCol)
 		i=0;j=0;
 		c = getc(newFile);
 		fpos_t pos;
-		int clean_read=0;
 		while(c!=EOF)
 		{
 			
 			if(c==SPACE)
 			{	
 				string[str_iterator] = TERMINATOR_CHAR;
-				if(str_iterator!=0)
-				{
-					matrix[i][j] = atoi(string);
-					for(str_iterator =0 ; str_iterator < BUFFER_SIZE ; str_iterator++) // zera o buffer
-						string[str_iterator] = 0;
-					str_iterator =0 ; // reseta o iterador da string
+				matrix[i][j] = atoi(string);
+				for(str_iterator =0 ; str_iterator < BUFFER_SIZE ; str_iterator++) // zera o buffer
+					string[str_iterator] = 0;
+				str_iterator =0 ; // reseta o iterador da string
 										
-					j++;
-				}
-				else
-				{
-					clean_read++;
-					printf("Received excessive SPACE on line %d, ignoring...\n",i+1);
-				}						
+				j++;
+				
+				
 			} else 
 			if(c==NEW_LINE)
-			{	
-				string[str_iterator] = TERMINATOR_CHAR;
-				if(str_iterator!=0)
-				{		
+			{
+				fgetpos(newFile, &pos);
+				c = getc(newFile);
+				fsetpos(newFile, &pos);
+				
+				if(c!=EOF && c!=NEW_LINE)
+				{
+					string[str_iterator] = TERMINATOR_CHAR;
 					matrix[i][j] = atoi(string);
+					
 					for(str_iterator =0 ; str_iterator < BUFFER_SIZE ; str_iterator++) // zera o buffer
 						string[str_iterator] = 0;
 					str_iterator =0 ; // reseta o iterador da string
-			
+					
 					i++;
 					j=0;
 				}
-				else
-				{
-					clean_read++;
-					printf("Received excessive NEW_LINE on line %d, ignoring...\n",i+1);
-				}
 			} else
-			if(is_int(c))
 			{
 				string[str_iterator] = c;
 				str_iterator++;
 			}
-			else
-			{
-				clean_read++;
-				printf("Received '%c', not an int, nor NEW_LINE, nor EOF on line %d, column %d\n",c,i+1,j+1);
-			}
 			c = getc(newFile);
 			
 		}
-		if(str_iterator!=0)
-		{
-			string[str_iterator] = TERMINATOR_CHAR;
-			//printf("%c",string[0]);
-			matrix[*nLine-1][*nCol-1] = atoi(string);
-		}
-		if(clean_read==0)
-			printf("Read OK!\n");
-		else
-			printf("%d errors...\n",clean_read);
+		string[str_iterator] = TERMINATOR_CHAR;
+		matrix[*nLine-1][*nCol-1] = atoi(string);
+		
 		fclose(newFile);
 		
 		return matrix;
